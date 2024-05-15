7bde53d7882bb6b899183259ac9f95f33db1538e
Dong Wen <dwen.uni@gmail.com>
2024-05-15 17:30:38 +1000

    nextjs export bug

--- 2024/_next/static/chunks/app/organisation/page-c37eadae7c66de7e.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[229,931],{788:function(e,n,s){Promise.resolve().then(s.bind(s,3552)),Promise.resolve().then(s.t.bind(s,8173,23))}},function(e){e.O(0,[834,173,552,971,23,744],function(){return e(e.s=788)}),_N_E=e.O()}]);
