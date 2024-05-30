35bb3d22615fbb984153ac069c2acb2780fcb661
Dong Wen <dwen.uni@gmail.com>
2024-05-30 12:01:49 +1000

    add shepherding submission link

---
+++ 2024/_next/static/chunks/app/attend/venue/page-58016881865de7a1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[945,653,857,416,765,686,495,929,600,974],{575:function(n,e,u){Promise.resolve().then(u.bind(u,3552))}},function(n){n.O(0,[117,552,971,23,744],function(){return n(n.s=575)}),_N_E=n.O()}]);
