8ba61e5886b8efeec25e33f3a28d426400ee15c3
DW <dwen.uni@gmail.com>
2024-06-06 20:25:07 +1000

    fix base_path link error

--- 2024/_next/static/chunks/app/program-committee/page-74c4ad04629ca4a5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974,857,495,765,686,416,945,653,929,600],{6694:function(n,e,u){Promise.resolve().then(u.bind(u,3552))}},function(n){n.O(0,[117,552,971,23,744],function(){return n(n.s=6694)}),_N_E=n.O()}]);
