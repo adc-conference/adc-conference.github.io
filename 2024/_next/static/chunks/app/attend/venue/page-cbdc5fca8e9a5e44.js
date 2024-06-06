09d4e4e8978ef0f801ebdc74ec47d405441cd71a
Dong Wen <dwen.uni@gmail.com>
2024-06-06 12:34:46 +1000

    add committee member

---
+++ 2024/_next/static/chunks/app/attend/venue/page-cbdc5fca8e9a5e44.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[945,416,653,857,765,495,686,929,974,600],{575:function(n,e,u){Promise.resolve().then(u.bind(u,3552))}},function(n){n.O(0,[117,552,971,23,744],function(){return n(n.s=575)}),_N_E=n.O()}]);
