5115b054467046e2c13148c2295cd28bc39ea806
Dong Wen <dwen.uni@gmail.com>
2024-06-06 12:54:22 +1000

    fix image bug

---
+++ 2024/_next/static/chunks/app/call-for-workshops/page-c0ec5b7f01b943b0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[765,857,416,653,945,495,686,974,929,600],{575:function(n,e,u){Promise.resolve().then(u.bind(u,3552))}},function(n){n.O(0,[117,552,971,23,744],function(){return n(n.s=575)}),_N_E=n.O()}]);
