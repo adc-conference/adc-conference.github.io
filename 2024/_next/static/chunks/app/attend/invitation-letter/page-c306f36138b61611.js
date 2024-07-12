09135a8973dafbbae63a9427f3b714e0f7013b8a
Dong Wen <dwen.uni@gmail.com>
2024-07-12 11:06:08 +1000

    fix image

---
+++ 2024/_next/static/chunks/app/attend/invitation-letter/page-c306f36138b61611.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[857,416,945,495,653,765,929,600,686,974],{575:function(n,e,u){Promise.resolve().then(u.bind(u,3552))}},function(n){n.O(0,[117,552,971,23,744],function(){return n(n.s=575)}),_N_E=n.O()}]);
