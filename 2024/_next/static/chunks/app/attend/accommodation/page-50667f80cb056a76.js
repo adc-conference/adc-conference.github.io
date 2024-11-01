c1e967e872d99d012ba49cabe02bead93c055145
adasken <duckhang.quach@gmail.com>
2024-11-01 19:07:43 +1000

    fix bottom homepage

--- 2024/_next/static/chunks/app/attend/accommodation/page-50667f80cb056a76.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[829,653,857,416,495,765,945,666,686,974,520,929,205,600,991],{9601:function(n,e,u){Promise.resolve().then(u.bind(u,3552))}},function(n){n.O(0,[949,552,971,23,744],function(){return n(n.s=9601)}),_N_E=n.O()}]);
