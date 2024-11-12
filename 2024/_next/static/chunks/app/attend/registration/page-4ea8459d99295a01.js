95acf537ce798d5d49a72fe85061531a9acf236c
adasken <duckhang.quach@gmail.com>
2024-11-12 15:38:39 +1000

    Add Japan Tutorials

--- 2024/_next/static/chunks/app/attend/registration/page-4ea8459d99295a01.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[653,857,829,416,495,765,945,686,666,520,974,600,929,205,991],{9601:function(n,e,u){Promise.resolve().then(u.bind(u,3552))}},function(n){n.O(0,[949,552,971,23,744],function(){return n(n.s=9601)}),_N_E=n.O()}]);
